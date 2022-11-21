93ed71aa386b0900e9338c699277227376a7a88e
nawal-afroun <115453579+nawal-afroun@users.noreply.github.com>
2022-11-21 21:44:59 +0100

    Update form-validation.js

--- tp3/js/form-validation.js
+++ tp3/js/form-validation.js
@@ -20,4 +20,5 @@ window.onload = function () {
 function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
-} 
+}
+
