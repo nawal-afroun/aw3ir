6242970f9fae9d16419adceab2709de1076f3df7
nawal-afroun <115453579+nawal-afroun@users.noreply.github.com>
2022-11-28 11:29:49 +0100

    f

--- tp4/js/form-validation.js
+++ tp4/js/form-validation.js
@@ -21,4 +21,7 @@ function validateEmail(email) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
+function calcNbChar(id) {
+  document.querySelector(`#${id} + span`).textContent = document.querySelector(`#${id}`).value.length;
+}
 
